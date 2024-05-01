d8a6c565313ba0ef632d63b1ca1330693d27dcb4
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 13:16:42 +0700

    Introduction to Python list comprehensions

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 if __name__ == "__main__":
-  print("Python List Comprehensions")
+  # Introduction to Python list comprehensions
+
+  numbers = [1, 2, 3, 4, 5]
+  print(numbers)
+
+  squares = []
+  for number in numbers:
+    squares.append(number ** 2)
+  print(squares)
+
+  numbers = [1, 2, 3, 4, 5]
+  print(numbers)
+
+  squares = list(map(lambda number: number ** 2, numbers))
+  print(squares)
+
+  numbers = [1, 2, 3, 4, 5]
+  print(numbers)
+
+  squares = [number ** 2 for number in numbers]
+  print(squares)
+
+  """
+  [output_expression for element in list]
+  """
